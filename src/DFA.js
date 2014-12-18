2ba8fd59beb81a8901b5afd9aaa43ed4e5283e03
pdaniell <pdaniell@mm.st>
2014-12-18 12:19:34 -0800

    adding to DFA

--- src/DFA.js
+++ src/DFA.js
@@ -33,6 +33,9 @@
             // We'll add to it later
             this.stateTable = new Machine.StateTable();
 
+            // The starting state
+            this.initialState = null; 
+
 
             // The current state of te machine. Since the state table
             // is empty this has to be null
@@ -61,7 +64,7 @@
 
             // Step counter: we haven't done anything yet so set 
             // the step count to 0
-            this.stepCounter = 0; 
+            this.stepCount = 0; 
 
             // Now we initialize the Transition Function. 
             this.transitionFunction = new Machine.TransitionFunction({
@@ -103,6 +106,230 @@
         setAlphabet: function(alphabet) {
             this.alphabet = alphabet;
         },
+
+        /**
+         * Retrieves the state table. 
+         * @method 
+         * @return {Machine.StateTable} The state table.
+         * 
+         */
+        getStateTable: function(){
+            return this.stateTable; 
+        },
+
+        /**
+         * Sets the state table. Using this method
+         * is not recommended since there are no internal
+         * checks for consistency. 
+         * @method
+         * @param {Machine.StateTable} stateTable The state table. 
+         */
+        setStateTable: function(stateTable){ 
+            this.stateTable = stateTable;
+        }, 
+
+
+        /**
+         * Retrieves the transition function.
+         * @method 
+         * @return {Machine.TransitionFunction} The transition function.
+         * 
+         */
+        getTransitionFunction: function(){
+            return this.transitionFunction;  
+        },
+
+        /**
+         * Sets the transition function. Using this method
+         * is not recommended since there are no internal
+         * checks for consistency. 
+         * @method
+         * @param {Machine.TransitionFunction} transitionFunction The transition function.
+         */
+        setTransitionFunction: function(transitionFunction){ 
+            this.transitionFunction = transitionFunction; 
+        }, 
+
+
+        /** 
+         * Retrieves the initial state. 
+         * @method 
+         * @return {Machine.State} The initial state.
+         */
+        getInitialState: function() {
+            return this.initialState;
+        },
+
+        /**
+         * Sets the initial state. 
+         * @method
+         * @param {Machine.State} initialState The initial state.
+         */
+        setInitialState: function(initialState) {
+            if (this.stateTable.contains(state) == false) {
+                throw new Error("Initial State is not in State Table");
+            }
+
+            this.initialState = initialState;
+
+        },
+
+        /**
+         * Sets the initial state with a label. 
+         * @method 
+         * @param {String} initialStateLabel The initial state.
+         */
+        setInitialStateByLabel: function(initialStateLabel) {
+            var state = this.stateTable.getStateByLabel(initialStateLabel); 
+            this.setInitialState(state); 
+        },
+
+
+        /** 
+         * Returns the current state. 
+         * @method
+         * 
+         * @return {Machine.State} The current state.
+         */
+        getCurrentState: function(){  
+            return this.currentState; 
+        }, 
+
+        /**
+         * Sets the current state.
+         * @method
+         * @param {Machine.State} currentState The new current state.
+         */
+         setCurrentState: function(currentState){
+            if (this.stateTable.contains(state) == false) {
+                throw new Error("Initial State is not in State Table");
+            }
+
+            this.currentState = currentState;   
+         }, 
+
+        /**
+         * Sets the current state label.
+         * @method
+         * @param {Machine.State} currentStateLabel The new current state.
+         */
+         setCurrentStateByLabel: function(currentStateLabel){
+            var state = this.stateTable.getStateByLabel(currentStateLabel); 
+            this.setCurrentState(state); 
+         }, 
+
+
+        /**
+         * Returns whether the DFA is in an accepting state. 
+         * @method 
+         * @return {Boolean} True if in accepted state.
+         */
+         getIsAccepted: function()  {
+            return this.isAccepted; 
+         }, 
+
+
+         /**
+          * Sets the value of the accepted state of the DFA.
+          * @method
+          * @param {Boolean} isAccepted The new accepted state.
+          * 
+          */
+         setIsAccepted: function(isAccepted){
+            this.isAccepted = isAccepted;
+         },
+
+
+
+        /**
+         * Returns whether the DFA is in a halting state. 
+         * @method 
+         * @return {Boolean} True if in halted state.
+         */
+         getIsHalted: function()  {
+            return this.isHalted; 
+         }, 
+
+
+         /**
+          * Sets the value of the halted state of the DFA.
+          * @method
+          * @param {Boolean} isHalted The new halted state.
+          * 
+          */
+         setIsHalted: function(isHalted){
+            this.isHalted = isHalted;
+         },
+
+
+         /**
+          * Returns the tape pointer position. 
+          * @method 
+          * @return {Number} The pointer position.
+          * 
+          */
+         getPointerPosition: function(){
+            return this.pointerPosition;
+         }, 
+
+         /**
+          * Sets the pointer position. 
+          * @method
+          * @param {Number} pointerPosition The pointer position.
+          */
+         setPointerPosition: function(pointerPosition){
+            this.pointerPosition = pointerPosition; 
+         },
+
+         /**
+          * Returns the step counter.
+          * @method 
+          * @return {Number} The step count.
+          * 
+          */
+         getStepCount: function(){
+            return this.stepCount;
+         }, 
+
+         /**
+          * Sets the step count
+          * @method
+          * @param {Number} stepCount The step count.
+          */
+         setStepCount: function(stepCount){
+            this.stepCount = stepCount;
+         },
+
+
+         /**
+          * Returns the tape object. 
+          * @method
+          * @return {Machine.Tape} The input tape.
+          */
+         getTape: function() { 
+            return this.tape;
+         }, 
+
+
+         /** 
+          * Sets the tape object.
+          * @method
+          * @param {Machine.Tape} tape The tape
+          */
+         setTape: function(tape){
+            this.tape = tape; 
+         }, 
+
+
+        // initialState
+        // currentState
+        // isAccepted
+        // isHalted
+        // tape
+        // pointerPosition
+        // stepCounter
+
+
 
 
         /**
@@ -155,28 +382,6 @@
 
         
 
-        /**
-         * [setInitialState description]
-         * @param {Machine.State} initialState [description]
-         */
-        setInitialState: function(initialState) {
-            if (this.stateTable.contains(state) == false) {
-                throw new Error("Initial State is not in State Table");
-            }
-
-            this.initialState = initialState;
-
-        },
-
-        /** @method **/
-        getInitialState: function() {
-            return this.initialState;
-        },
-
-        /** @method **/ 
-        getCurrentState: function(){  
-            return this.currentState; 
-        }, 
 
         /** @method **/
         reset: function() {
@@ -184,36 +389,6 @@
             this.inputIndex = 0;
         },
 
-        /** @method **/
-        setInput: function(input) {
-            this.input = input;
-
-        },
-
-        /** @method **/
-        getInput: function() {
-            return this.input;
-        },
-
-        /** #method **/
-        setInputIndex: function(inputIndex) {
-            this.inputIndex = inputIndex;
-        },
-
-        /** @method **/ 
-        incrementInputIndex: function() { 
-
-        },
-
-        /** @method **/
-        getInputIndex: function() {
-            return this.inputIndex;
-        },
-
-        /** @method **/
-        getCurrentCharacter: function() {
-            return this.getInput(this.getInputIndex());
-        },
 
 
         /** @method **/
